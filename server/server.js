da67af72fb2c6a053f837f4262a135bfba9eea3e
LastL1fe <jarellh98@gmail.com>
2022-01-05 22:04:47 -0500

    ye

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
+const express = require('express')
+
 const port = 8000;
 
 const server = http.createServer(function(req, res){
+    console.log(`request was made from: ${req.socket.remoteAddress}`)
     res.writeHead(200, {'Content-Type':'text/html'});
-    res.write('fuck me right in the ass');
+    //res.write('fuck me right in the ass');
+    res.write('ding dong i have your ip address now smiley face :)')
     res.end();
 })
 
