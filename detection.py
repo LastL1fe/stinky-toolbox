4e5a6ab269add231e1c82803e2b7715978929adc
LastL1fe <jarellh98@gmail.com>
2022-01-29 00:20:39 -0500

    i have hepititis :(

--- detection.py
+++ detection.py
@@ -1,8 +1,7 @@
-from base64 import encode
+import asyncio
+import aiohttp
 import cv2 as cv
 import numpy as np
-from requests.models import encode_multipart_formdata
-import sizematters
 import threading
 import requests
 
@@ -10,20 +9,22 @@ processFrame = True
 
 def video(cam):
     global processFrame
+
     t = threading.Timer(2, scuffedCountDown)
+    faceCascade = cv.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
 
     while 1:
         #Captures every frame of the video, processes it and then display it
         _, cum = cam.read()
         cum = cv.flip(cum, 1)
         grayScale = cv.cvtColor(cum, cv.COLOR_BGR2GRAY)
-        face = detectFace(grayScale)
+        face = detectFace(grayScale, faceCascade)
 
         if np.any(face) and processFrame:
             not processFrame
-            
-            newThread = threading.Thread(target=faceRecog, args=(cum,), daemon=True)
-            newThread.start()
+            saveFace(cum, faceCascade)
+
+
         elif not np.any(face) and not processFrame and not t.is_alive():
             t.start()
         elif np.any(face) and not processFrame and t.is_alive():
@@ -33,21 +34,28 @@ def video(cam):
         if cv.waitKey(10) == 27:
             break
 
-def detectFace(gray): 
-    faceCascade = cv.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
+def detectFace(gray, faceCascade):
     face = faceCascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=15)
     return face
 
-def faceRecog(img):
+def saveFace(img, faceCascade):
+    face = faceCascade.detectMultiScale(img, scaleFactor=1.05, minNeighbors=15)
+    for (x, y, w, h) in face:
+        img = img[x:x+w, y:y+h]
+        cv.imwrite("temp/temp.png", img)
+
+def faceRecog():
     params = {
         "api_key": "UN5V5OR6z1c0cqj4k_-HZI8q1DBoEwq0",
         "api_secret": "-jVnsCfNvC-FG_3iGU1WHUMv5O6fIose",
-        "face_token1": "a84e77fbbe11cff01cbdaaec4f36f2d9",
+        "image_url1": "http://76.234.138.246:8000/img/faceass.png",
     }
-    verifyFaceRequest = requests.request(encode_multipart_formdata)
+    requests.post(url="https://api-us.faceplusplus.com/facepp/v3/compare", params=params, files={"image_file2": open("temp/temp.png", "rb")})
 
-def drawFaceDetections():
-    pass
+def logFace():
+    url = "http://76.234.138.246:8000/upload"
+    files = {"image_file2": open("temp/temp.png", "rb")}
+    requests.post(url=url, files=files)
 
 def scuffedCountDown():
     global processFrame
@@ -55,6 +63,9 @@ def scuffedCountDown():
     if not processFrame:
         processFrame = True
     threading.RLock.release()
+
+def drawFaceDetections():
+    print("working")
 
 # x1 = settings[2][1] + (x / settings[0][0])
 # y1 = settings[2][3] - (y / settings[0][1])
